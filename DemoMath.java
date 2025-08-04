d74e5659d017e782087d07ab565e0c8574f916e1
Victor Yiu <yiulokhei91@gmail.com>
2025-08-04 18:09:17 +0800

    update

---
+++ DemoMath.java
@@ -0,0 +1,7 @@
+public class DemoMath {
+  public static void main(String[] args) {
+    int x = 5;
+    int y = 9;
+    System.out.println(x + y); //14 (int) -> "14"
+  }
+}
---
+++ Helloworld.java
@@ -0,0 +1,19 @@
+public class Helloworld {
+  public static void main(String[] args) {
+    System.out.println("hello world");
+    System.out.println("I' Victor");
+    //comment / Notes
+    //Java Machine will ignores lines starting with //
+    //Data Type (int)
+    int x = 3;
+    int y = 7;
+    double radius = 3.4;// double is to store decimal numbers
+    System.out.println(x);
+    System.out.println(y);
+    System.out.println(radius);
+    int a = 10;
+    System.out.println(a);
+    a = 17;
+    System.out.println(a);
+  }
+}
