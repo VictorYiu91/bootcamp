865b125af6741b2cf9505f6b2e3d185553f5c5d1
Victor Yiu <yiulokhei91@gmail.com>
2025-08-06 10:03:14 +0800

    202508061003

--- DemoMath.java
+++ DemoMath.java
@@ -3,5 +3,9 @@ public class DemoMath {
     int x = 5;
     int y = 9;
     System.out.println(x + y); //14 (int) -> "14"
+    System.out.println(y-x);
+    System.out.println(x*y);
+    x = 3;
+    System.out.println(y/x);
   }
 }
