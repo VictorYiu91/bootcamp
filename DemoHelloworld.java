98391313ce29375cd66a7f261af4c63b5e6e43ef
Victor Yiu <yiulokhei91@gmail.com>
2025-08-08 15:42:41 +0800

    202508081542

--- DemoHelloworld.java
+++ DemoHelloworld.java
@@ -1,7 +1,7 @@
-public class Helloworld {
+public class DemoHelloworld {
   public static void main(String[] args) {
     System.out.println("hello world");
-    System.out.println("I' Victor");
+    System.out.println("I'm Victor");
     //comment / Notes
     //Java Machine will ignores lines starting with //
     //Data Type (int)
